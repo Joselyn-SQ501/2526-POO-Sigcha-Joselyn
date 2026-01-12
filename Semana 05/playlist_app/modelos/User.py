3a0d705721fd510f04a8d16b05a032791b9cf56b
Joselyn Sigcha <ja.sigchaq@uea.edu.ec>
2026-01-11 20:49:02 -0500

    Tarea S05 - Subclase User del Analizador de Playlists 🎵

---
+++ Semana 05/playlist_app/modelos/User.py
@@ -0,0 +1,5 @@
+# Subclase para ingresar datos sobre el usuario
+class User:
+    # Creación de un constructor vacío
+    def __init__(self):
+        self.user_type = "" # Atributo para ingresar el tipo de usuario como tipo de dato string.
