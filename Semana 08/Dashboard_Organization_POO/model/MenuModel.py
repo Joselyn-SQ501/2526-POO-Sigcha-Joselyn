90289516a48849bbd3e961e479b0b3e584ed2eca
Joselyn Sigcha <ja.sigchaq@uea.edu.ec>
2026-02-01 18:41:24 -0500

    Tarea S08 - Clase menú de la carpeta model de la organización modular del Dashboard.py

---
+++ Semana 08/Dashboard_Organization_POO/model/MenuModel.py
@@ -0,0 +1,8 @@
+# Creación de una clase para manejar el menú de unidades
+class MenuModel:
+    # Constructor vacío que inicializa las unidades disponibles
+    def __init__(self):
+        self.unidades = {
+            '1': 'Unidad 1', # Uso como atributo de la carpeta unidad 1 del proyecto POO, como parte de la modularización del archivo Dashboard.py
+            '2': 'Unidad 2' # Uso como atributo de la carpeta unidad 2 del proyecto POO, como parte de la modularización del archivo Dashboard.py
+        }
